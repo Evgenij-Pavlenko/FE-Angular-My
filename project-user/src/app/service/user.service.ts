4ed20a4e3dd180b270793775425b91fe479a8770
Evgenij Pavlenko <evgenij.pavlenko@gmail.com>
2020-09-16 00:54:37 +0200

    filter correct -observer and pipe2

---
+++ project-user/src/app/service/user.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {User} from '../model/user';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  users: User[];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>('assets/users.json');
+  }
+
+  getUserById(id: string): Observable<User> {
+    return this.getUsers().pipe(
+      map(users => users.find(user => user.id === id))
+    );
+  }
+
+  getFilteredUsers(filter: { key: string, value: any }[]): User[] {
+    const filteredUsers: User[] = [];
+    this.getUsers().forEach(user => {
+      filter.forEach(element => {
+        if (user[element.key].toString() === element.value.toString()) {
+          filteredUsers.push(user);
+        }
+      });
+    });
+    return filteredUsers;
+  }
+}
